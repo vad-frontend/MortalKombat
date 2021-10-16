6900cc2fe36771573f5be9ee19495f2dec98ea45
Vadim Filippov <v.filippov@ntechlab.com>
2021-10-16 13:27:13 +0300

    homework 3

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const $divArenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+let isGameOver = false;
+
 const player1 = {
+    player: 1,
     name: 'Scorpion',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -9,6 +14,7 @@ const player1 = {
 }
 
 const player2 = {
+    player: 2,
     name: 'Sub-zero',
     hp: 90,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
@@ -17,20 +23,22 @@ const player2 = {
         console.log(this.name + ' fight...')
     }
 }
-const $divArenas = document.querySelector('.arenas');
-function createPlayer(playerClass, playerObject) {
-    const $divPlayer = document.createElement('div'),
-        $divProgressBar = document.createElement('div'),
-        $divLife = document.createElement('div'),
-        $divName = document.createElement('div'),
-        $divCharacter = document.createElement('div'),
-        $img = document.createElement('img');
 
-    $divPlayer.classList.add(playerClass);
-    $divProgressBar.classList.add('progressbar');
-    $divLife.classList.add('life');
-    $divName.classList.add('name');
-    $divCharacter.classList.add('character')
+function createElement(tagName, className) {
+    const $el = document.createElement(tagName)
+
+    if (className) $el.classList.add(className);
+
+    return $el;
+}
+
+function createPlayer(playerObject) {
+    const $divPlayer = createElement('div', `player${playerObject.player}`),
+        $divProgressBar = createElement('div', 'progressbar'),
+        $divLife = createElement('div', 'life'),
+        $divName = createElement('div', 'name'),
+        $divCharacter = createElement('div', 'character'),
+        $img = createElement('img');
 
     $divLife.style.width = `${playerObject.hp}%`;
     $divName.innerText = playerObject.name;
@@ -41,8 +49,37 @@ function createPlayer(playerClass, playerObject) {
     $divProgressBar.appendChild($divName);
     $divPlayer.appendChild($divProgressBar);
     $divPlayer.appendChild($divCharacter);
-    $divArenas.appendChild($divPlayer);
+    return $divPlayer;
 }
 
-createPlayer('player1', player1);
-createPlayer('player2', player2);
+function gameOver(player) {
+    $divArenas.appendChild(playerLose(player))
+    isGameOver = true;
+    $randomButton.disabled = true;
+}
+
+function changeHP(player) {
+    if (isGameOver) return;
+    const $playerLife = document.querySelector(`.player${player.player} .life`);
+    const damage = Math.ceil(Math.random() * 10);
+    player.hp = player.hp <= 0 ? 0 : player.hp -= damage;
+    $playerLife.style.width = `${player.hp}%`;
+
+    if (player.hp === 0) gameOver(player);
+}
+
+function playerLose(player) {
+    const whoWin = player.player === 1 ? player2 : player1;
+    const $loseTitle = createElement('div', 'loseTitle');
+    $loseTitle.innerText = `${whoWin.name} win`;
+
+    return $loseTitle;
+}
+
+$randomButton.addEventListener('click', function () {
+    const whoAttack = Math.random() > 0.5 ? player1 : player2;
+    changeHP(whoAttack);
+})
+
+$divArenas.appendChild(createPlayer(player1));
+$divArenas.appendChild(createPlayer(player2));
